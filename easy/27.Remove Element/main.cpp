71e12e17e4705dd75d488d1910f591272b2873a8
Kingpie <jinpei618@126.com>
2020-03-21 22:26:29 +0800

    Remove Element

---
+++ easy/27.Remove Element/main.cpp
@@ -0,0 +1,45 @@
+/*
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example 1:
+
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,1,2,2,3,0,4,2], val = 2,
+
+Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+
+Note that the order of those five elements can be arbitrary.
+
+It doesn't matter what values are set beyond the returned length.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+//head pointer & tail pointer
+class Solution1 {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int start = 0;
+        int end = nums.size();
+        while(start < end){
+            if(nums[start] == val){
+                nums[start] = nums[end-1];
+                end--;
+            }else{
+                start++;
+            }
+        }
+        return end;
+    }
+};
