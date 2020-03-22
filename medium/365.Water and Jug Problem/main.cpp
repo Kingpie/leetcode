eda9d7fd39e96c5b4ebd454f0239248f418ab7b4
Kingpie <jinpei618@126.com>
2020-03-22 11:37:09 +0800

    Water and Jug Problem

---
+++ medium/365.Water and Jug Problem/main.cpp
@@ -0,0 +1,33 @@
+/*You are given two jugs with capacities x and y litres. There is an infinite amount of water supply available. You need to determine whether it is possible to measure exactly z litres using these two jugs.
+
+If z liters of water is measurable, you must have z liters of water contained within one or both buckets by the end.
+
+Operations allowed:
+
+Fill any of the jugs completely with water.
+Empty any of the jugs.
+Pour water from one jug into another till the other jug is completely full or the first jug itself is empty.
+Example 1: (From the famous "Die Hard" example)
+
+Input: x = 3, y = 5, z = 4
+Output: True
+Example 2:
+
+Input: x = 2, y = 6, z = 5
+Output: False
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/water-and-jug-problem
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+//Bézout's lemma
+class Solution {
+public:
+    bool canMeasureWater(int x, int y, int z) {
+        if (x + y < z)
+            return false;
+        if (x == 0 || y == 0)
+            return z == 0 || x + y == z;
+        return z % gcd(x, y) == 0;
+    }
+};
