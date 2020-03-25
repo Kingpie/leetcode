a3f717d462ed21350d53c6254276ecaffcf25a94
Kingpie <jinpei618@126.com>
2020-03-25 14:39:54 +0800

    Best Time to Buy and Sell Stock II

--- 122.Best Time to Buy and Sell Stock II/main.cpp
+++ 122.Best Time to Buy and Sell Stock II/main.cpp
@@ -22,7 +22,8 @@ Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-
 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
 
-class Solution {
+//greedy
+class Solution1 {
 public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
@@ -33,3 +34,33 @@ public:
         return profit;
     }
 };
+
+int len = prices.length;
+if (len < 2) {
+return 0;
+}
+
+//dp
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        // 0：have cash
+        // 1：have stock
+        int len = prices.size();
+        if(len == 0)
+            return 0;
+
+        auto dp = new int[len][2]{};
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < len; i++) {
+
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[len - 1][0];
+    }
+};
+
