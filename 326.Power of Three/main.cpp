65aa6d588bdefb49c361b759c6f4e92fcbfab99a
Kingpie <jinpei618@126.com>
2020-03-30 11:37:50 +0800

    Power of Two

---
+++ 326.Power of Three/main.cpp
@@ -0,0 +1,44 @@
+/*Given an integer, write a function to determine if it is a power of three.
+
+Example 1:
+
+Input: 27
+Output: true
+Example 2:
+
+Input: 0
+Output: false
+Example 3:
+
+Input: 9
+Output: true
+Example 4:
+
+Input: 45
+Output: false
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/power-of-three
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+//loop
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n == 0) return false;
+        while(n%3==0){
+            n /= 3;
+        }
+        return n == 1;
+    }
+};
+
+//log
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n <= 0) return false;
+        double res = log10(n)/log10(3);
+        return res -int(res) == 0;
+    }
+};
