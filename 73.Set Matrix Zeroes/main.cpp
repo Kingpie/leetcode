b9613bc346cb4a1492e479dbffd968d02553f8f9
Kingpie <jinpei618@126.com>
2020-04-06 23:33:40 +0800

    hash

--- 73.Set Matrix Zeroes/main.cpp
+++ 73.Set Matrix Zeroes/main.cpp
@@ -33,7 +33,7 @@ Output:
 链接：https://leetcode-cn.com/problems/set-matrix-zeroes
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
 
-//hash
+//hash function
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
