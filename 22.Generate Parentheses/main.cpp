6fbb6798778a28fccce23a01eb987e183143b28d
Kingpie <jinpei618@126.com>
2020-04-09 23:42:00 +0800

    dfs

---
+++ 22.Generate Parentheses/main.cpp
@@ -0,0 +1,36 @@
+/*Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/generate-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+//dfs
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        int lc = 0, rc = 0;
+        dfs(res, "", n, lc, rc);
+        return res;
+    }
+    void dfs(vector<string>& res, string path, int n, int lc, int rc) {
+        if (rc > lc || lc > n || rc > n) return;
+        if (lc == rc && lc == n) {
+            res.push_back(path);
+            return;
+        }
+        dfs(res, path + '(', n, lc + 1, rc);
+        dfs(res, path + ')', n, lc, rc + 1);
+    }
+};
+
