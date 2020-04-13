de41e367b541945d0c7785513f72f2c5315f3263
Kingpie <jinpei618@126.com>
2020-04-13 19:16:59 +0800

    two pointers

---
+++ 328.Odd Even Linked List/main.cpp
@@ -0,0 +1,57 @@
+/*Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL
+Output: 1->3->5->2->4->NULL
+Example 2:
+
+Input: 2->1->3->5->6->4->7->NULL
+Output: 2->3->6->7->1->5->4->NULL
+Note:
+
+The relative order inside both the even and odd groups should remain as it was in the input.
+The first node is considered odd, the second node even and so on ...
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/odd-even-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        ListNode* odd = new ListNode(0);
+        ListNode* even = new ListNode(0);
+
+        auto tempOdd = odd;
+        auto tempEven = even;
+        int cnt = 1;
+        while(head){
+            if((cnt&1)==1){
+                tempOdd->next = head;
+                tempOdd = tempOdd->next;
+            }else{
+                tempEven->next = head;
+                tempEven = tempEven->next;
+            }
+            cnt++;
+            head = head->next;
+        }
+
+        if(tempEven)
+            tempEven->next = nullptr;
+
+        tempOdd->next = even->next;
+        return odd->next;
+    }
+};
