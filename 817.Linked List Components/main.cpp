a6dddc8f4539eeb628128b0079f49a1381d4ab8c
Kingpie <jinpei618@126.com>
2020-04-13 21:09:58 +0800

    pointer

---
+++ 817.Linked List Components/main.cpp
@@ -0,0 +1,54 @@
+/*We are given head, the head node of a linked list containing unique integer values.
+
+We are also given the list G, a subset of the values in the linked list.
+
+Return the number of connected components in G, where two values are connected if they appear consecutively in the linked list.
+
+Example 1:
+
+Input:
+head: 0->1->2->3
+G = [0, 1, 3]
+Output: 2
+Explanation:
+0 and 1 are connected, so [0, 1] and [3] are the two connected components.
+Example 2:
+
+Input:
+head: 0->1->2->3->4
+G = [0, 3, 1, 4]
+Output: 2
+Explanation:
+0 and 1 are connected, 3 and 4 are connected, so [0, 1] and [3, 4] are the two connected components.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/linked-list-components
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& G) {
+        unordered_set<int> st(G.begin(),G.end());
+        int cnt = 0,ans = 0;
+        while(head){
+            if(st.count(head->val)){
+                cnt++;
+            }else{
+                if(cnt)
+                    ans++;
+                cnt = 0;
+            }
+            head = head->next;
+        }
+        return cnt>0?ans+1:ans;
+    }
+};
+
