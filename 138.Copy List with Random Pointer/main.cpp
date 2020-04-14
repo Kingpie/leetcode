b4211109e3d86fb22a2ec17ffa86e9a5e366f9ce
Kingpie <jinpei618@126.com>
2020-04-14 20:28:25 +0800

    hashmap

---
+++ 138.Copy List with Random Pointer/main.cpp
@@ -0,0 +1,76 @@
+/*A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+The Linked List is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+
+val: an integer representing Node.val
+random_index: the index of the node (range from 0 to n-1) where random pointer points to, or null if it does not point to any node.
+ 
+
+Example 1:
+
+
+Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Example 2:
+
+
+Input: head = [[1,1],[2,1]]
+Output: [[1,1],[2,1]]
+Example 3:
+
+
+
+Input: head = [[3,null],[3,0],[3,null]]
+Output: [[3,null],[3,0],[3,null]]
+Example 4:
+
+Input: head = []
+Output: []
+Explanation: Given linked list is empty (null pointer), so return null.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/copy-list-with-random-pointer
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        auto temp = head;
+        auto pre = nullptr;
+        unordered_map<Node*,Node*> mp;
+        while(temp){
+            mp[temp] = new Node(temp->val);
+            temp = temp->next;
+        }
+
+        temp = head;
+        while(temp){
+            if(temp->next){
+                mp[temp]->next = mp[temp->next];
+            }
+            if(temp->random){
+                mp[temp]->random = mp[temp->random];
+            }
+            temp = temp->next;
+        }
+        return mp[head];
+    }
+};
