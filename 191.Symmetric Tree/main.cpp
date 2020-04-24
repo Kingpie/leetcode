897b36936e1d88df764cecd221c1922bfc44e476
Kingpie <jinpei618@126.com>
2020-04-24 23:02:04 +0800

    dfs

---
+++ 191.Symmetric Tree/main.cpp
@@ -0,0 +1,48 @@
+/*Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ 
+
+Follow up: Solve it both recursively and iteratively.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/symmetric-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool dfs(TreeNode* left,TreeNode* right){
+        if(left == NULL && right == NULL) return true;
+        if(left == NULL || right == NULL) return false;
+        if(left->val!=right->val) return false;
+        return dfs(left->left,right->right)&&dfs(right->left,left->right);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        return dfs(root->left,root->right);
+    }
+};
