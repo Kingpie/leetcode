d34f5e25e7dedb2231849df55166dca06e50362b
Kingpie <jinpei618@126.com>
2020-04-25 10:40:56 +0800

    recursion

---
+++ 590.N-ary Tree Postorder Traversal/main.cpp
@@ -0,0 +1,58 @@
+/*Given an n-ary tree, return the postorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+ 
+
+Follow up:
+
+Recursive solution is trivial, could you do it iteratively?
+
+ 
+
+Example 1:
+
+
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [5,6,3,2,4,1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    vector<int> ans;
+public:
+    void dfs(Node* node){
+        for(int i = 0; i < node->children.size();++i){
+            dfs(node->children[i]);
+        }
+        ans.push_back(node->val);
+    }
+    vector<int> postorder(Node* root) {
+        if(!root) return {};
+        dfs(root);
+        return ans;
+    }
+};
