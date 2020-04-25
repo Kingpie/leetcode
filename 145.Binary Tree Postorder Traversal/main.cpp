22668d8f9fe1bd53054828ade2150d9f499b75d1
Kingpie <jinpei618@126.com>
2020-04-25 10:34:35 +0800

    stack

---
+++ 145.Binary Tree Postorder Traversal/main.cpp
@@ -0,0 +1,52 @@
+/*Given a binary tree, return the postorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [3,2,1]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-postorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s1,s2;
+        vector<int> ans;
+        if(root)
+            s1.push(root);
+
+        while(!s1.empty()){
+            auto node = s1.top();
+            s1.pop();
+            s2.push(node);
+
+            if(node->left)
+                s1.push(node->left);
+            if(node->right)
+                s1.push(node->right);
+        }
+        while(!s2.empty()){
+            ans.push_back(s2.top()->val);
+            s2.pop();
+        }
+        return ans;
+    }
+};
