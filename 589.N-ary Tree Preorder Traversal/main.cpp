5505800f428793e915c9d44c954a2d6a44cbe958
Kingpie <jinpei618@126.com>
2020-04-25 10:14:03 +0800

    stack

---
+++ 589.N-ary Tree Preorder Traversal/main.cpp
@@ -0,0 +1,68 @@
+/*Given an n-ary tree, return the preorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+ 
+
+Follow up:
+
+Recursive solution is trivial, could you do it iteratively?
+
+ 
+
+Example 1:
+
+
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [1,3,5,6,2,4]
+Example 2:
+
+
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        stack<Node*> stk;
+        vector<int> ans;
+        if(root)
+            stk.push(root);
+        while(!stk.empty()){
+            auto node = stk.top();
+            stk.pop();
+            ans.push_back(node->val);
+            for(int i = node->children.size()-1; i >=0;i--){
+                if(node->children[i])
+                    stk.push(node->children[i]);
+            }
+        }
+        return ans;
+    }
+};
