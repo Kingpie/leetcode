3236a2968c8662858b6db6d94b944fdc4eec6430
Kingpie <jinpei618@126.com>
2020-04-25 10:08:47 +0800

    stack

---
+++ 144.Binary Tree Preorder Traversal/main.cpp
@@ -0,0 +1,47 @@
+/*Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> stk;
+        vector<int> ans;
+        if(root)
+            stk.push(root);
+        while(!stk.empty()){
+            auto node = stk.top();
+            stk.pop();
+            ans.push_back(node->val);
+
+            if(node->right)
+                stk.push(node->right);
+            if(node->left)
+                stk.push(node->left);
+        }
+        return ans;
+    }
+};
