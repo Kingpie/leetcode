f335f0d5ad56cc00629fce38fb8b5b55dd910ffa
Kingpie <jinpei618@126.com>
2020-04-26 22:42:07 +0800

    binary

--- 35.Search Insert Position/main.cpp
+++ 35.Search Insert Position/main.cpp
@@ -35,3 +35,20 @@ public:
         return i;
     }
 };
+
+//binary
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start = 0,end=nums.size();
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(target>nums[mid]){
+                start=mid+1;
+            }else{
+                end=mid;
+            }
+        }
+        return start;
+    }
+};
