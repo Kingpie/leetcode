f6b971be9717745ca0ab96223497d525f7477b24
Kingpie <jinpei618@126.com>
2020-05-12 23:16:52 +0800

    delete comments

--- 66.Plus One/main.cpp
+++ 66.Plus One/main.cpp
@@ -19,7 +19,6 @@ Explanation: The array represents the integer 4321.
 链接：https://leetcode-cn.com/problems/plus-one
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
 
-//defend double 100% players
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
